de679a5b09f399c0af84f8f317f4351b0b779a9f
Juan Schandin <jschandin@gmail.com>
2016-12-05 01:08:12 -0300

    Agregar el scraper de Zonacrap

--- scrapers.py
+++ scrapers.py
@@ -28,7 +28,60 @@ class Scraper(object):
         return r.text
 
 
-class Argenscrap(Scraper):
+class Zonacrap(Scraper):
+
+    URL = 'http://www.zonaprop.com.ar'
+
+    def __init__(self, url):
+        Scraper.__init__(self, url)
+        self.soup = self.get_soup()
+
+    def get_soup(self):
+        html = bs4.BeautifulSoup(self.request(), 'html.parser')
+        return html.find_all('li', {'class': 'post'})
+
+    def scrap(self):
+        cols = [
+            self.get_title(),
+            self.get_address(),
+            self.get_price(),
+            self.get_info(),
+            self.get_url()
+        ]
+
+        rows = zip(*cols)
+        return [House(*row) for row in rows]
+
+    def get_title(self):
+        return [e.find('h4').get_text().strip()
+                for e in self.soup]
+
+    def get_address(self):
+        return [e.find('div', {'class': 'post-text-location'})
+                 .get_text()
+                 .strip()
+                for e in self.soup]
+
+    def get_info(self):
+        return [e.find('ul', {'class': 'misc unstyled'})
+                 .get_text()
+                 .strip()
+                for e in self.soup]
+
+    def get_price(self):
+        return [e.find('span', {'class': 'precio-valor'})
+                 .get_text()
+                 .strip()
+                for e in self.soup]
+
+    def get_url(self):
+        return [self.URL +
+                e.find('div', {'class': 'post-text-desc'})
+                 .find('a').get('href')
+                for e in self.soup]
+
+
+class Argencrap(Scraper):
 
     URL = 'http://www.argenprop.com'
 
