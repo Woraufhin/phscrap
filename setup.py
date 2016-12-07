37aec672244ea47aec812023bd32602d409504fe
juan.schandin <juan.schandin@avature.net>
2016-12-07 13:16:11 -0300

    Improving setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='phscrap',
     version='0.1',
     author='Juan Schandin',
     author_email='jschandin@gmail.com',
-    py_modules=['phscrap'],
+    packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
     entry_points="""
     [console_scripts]
-    phscrap = phscrap:main
+    phscrap = scrap.phscrap:main
     """
 )
